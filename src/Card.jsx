3f676d152a9701a94e6482a55f1598bd6375eeeb
juliolantemona <juliolantemona7@gmail.com>
2023-04-01 12:24:34 +0700

    pertemuan6

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,13 @@
 function Card() {
     return (
         <>
-            <div class="flex justify-center items-center h-screen w-full bg-gradient-to-r from-green-500 to-green-600">
-                <div class="w-1/2 h-5/4 bg-white rounded-lg shadow-2xl p-8 m-8">
-                    <h1 class="block w-full text-center tect-gray-800 text-3xl font-bold mb-2">
-                        KTP
+            <div class="flex justify-center items-center h-screen w-full bg-gradient-to-r from-blue-500 to-purple-600">
+                <div class="background-blur-md w-1/2 bg-white bg-opacity-25 rounded-lg shadow-2xl p-8 m-4">
+                    <h1 class="block w-full text-center tect-gray-800 text-2xl font-bold mb-6">
+                        Registarsi
                     </h1>
                     <form action="/" method="post">
                         <div class="flex flex-col mb-4">
-                        <img class="rounded -lg w-40 h-60" src="j.jpg"/>
                             <label
                                 class="mb-2 font-bold text-lg text-gray-900"
                                 for="nama"
@@ -19,68 +18,72 @@ function Card() {
                                 class="border py-2 px-3 text-grey-800"
                                 type="text"
                                 name="nama"
-                                id="naam"
+                                id="nama"
                             />
-                             <label
+                            </div>
+                            <div class="flex flex-col mb-4">
+                                <label
                                 class="mb-2 font-bold text-lg text-gray-900"
-                                for="Tempat Lahir"
-                            >
-                                Tempat Lahir
-                            </label>
-                            <input
-                                class="border py-2 px-3 text-grey-800"
-                                type="text"
-                                name="Tempat Lahir"
-                                id="naam"
-                            />
+                                for="gender"
+                                >
+                                    Gender
+                                </label>
+                            <div>
+                                <input
+                                class="border py-2 text-gray-800"
+                                type="radio"
+                                name="gender"
+                                id="pria"
+                                />
+                                <label
+                                class="mb-2 text-gray-900"
+                                for="pria"
+                                > Pria</label>
+                            </div>
+                            <div>
+                                <input
+                                class="border py-2 text-grey-800"
+                                type="radio"
+                                name="gender"
+                                id="wanita"
+                                />
+                                <label
+                                class="mb-2 text-gray-900"
+                                for="wanita"
+                                > Wanita</label>
+                            </div>
+                        </div>
+                        <div class="flex flex-col mb-4">
                             <label
-                                class="mb-2 font-bold text-lg text-gray-900"
-                                for="Tgl Lahir"
-                            >
-                                Tgl Lahir
-                            </label>
-                            <input
-                                class="border py-2 px-3 text-grey-800"
-                                type="text"
-                                name="Tgl Lahir"
-                                id="naam"
-                            />
-                            <label
-                                class="mb-2 font-bold text-lg text-gray-900"
-                                for="Jenis Kelamin"
-                            >
-                                Jenis Kelamin
-                            </label>
-                            <input
-                                class="border py-2 px-3 text-grey-800"
-                                type="text"
-                                name="Jenis Kelamin"
-                                id="naam"
-                            />
-                            <label
-                                class="mb-2 font-bold text-lg text-gray-900"
-                                for="Agama"
+                            class="mb-2 font-bold text-lg text-gray-900"
+                            for="agama"
                             >
                                 Agama
                             </label>
-                            <input
-                                class="border py-2 px-3 text-grey-800"
-                                type="text"
-                                name="Agama"
-                                id="naam"
-                            />
+                            <select
+                            class="border py-2 px-3 text-gray-800"
+                            name="agama"
+                            id="agama"
+                            >
+                                <option value="Protestan">Kristen Protestan</option>
+                                <option value="Katholik">Katholik</option>
+                                <option value="Islam">Islam</option>
+                                <option value="Hindu">Hindu</option>
+                                <option value="Budha">Budha</option>
+                                <option value="Kepercayaan">Aliran Kepercayaan</option>
+                            </select>
                         </div>
                         <button
-                        class="block bg-blue-500 hover:bg-blue-200 text-white uppercase text-lg mx-auto p-1 rounded"
+                        class="block bg-blue-600 hover:bg-blue-00 text-white uppercase text-lg mx-auto p-4 rounded"
                         type="submit"
                         >
                             Buat Akun
                         </button>
-                    </form>
-                    <a
-                        class="block w-full text-center no-underline mt- text-sm text-gray-700 hover:text-gry-900"
+                        </form>
+                        <a
+                        class="block w-full text-center no-underline mt-4 text-sm text-gray-700 hover:text-gray-900"
                         href="/"
-                    >
+                        >
                         Telah Memiliki Akun?
                     </a>
                 </div>
